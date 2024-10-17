8dbb3837e21d382f78a5bc047d1411fc40e19350
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-10-18 12:28:21 +1300

    Simulate a dataset

---
+++ simulate.py
@@ -0,0 +1,28 @@
+# Generate a simulated dataset
+
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.random as rng
+
+rng.seed(123)
+a = 0.99
+sigma = 1.0
+b = sigma*np.sqrt(1.0 - a**2)
+
+t = np.linspace(0.0, 1000.0, 201)
+y = np.zeros(t.shape)
+
+y[0] = sigma*rng.randn()
+for i in range(1, len(t)):
+    y[i] = a*y[i-1] + b*rng.randn()
+y += 0.1*rng.randn(len(t))
+
+data = np.empty((len(t), 3))
+data[:,0] = t
+data[:,1] = y
+data[:,2] = 0.1
+np.savetxt("simulated_data.txt", data)
+
+plt.errorbar(t, y, yerr=0.1, fmt="o")
+plt.show()
+
