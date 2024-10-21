e32d73bc7dd4f7ad574e2140111d17cfdedb7923
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-10-22 12:53:28 +1300

    Make it a function

--- extract_data.py
+++ extract_data.py
@@ -7,35 +7,44 @@ import numpy as np
 fits_file = fits.open("TotalDat.fits")
 totaldat = fits_file[1].data
 
-qso_number = 0
-band = "g"
+def get_data(qso_number, band):
+    """
+    Band must be 'g', 'r' or 'i'.
+    """
 
-t = totaldat[f"MJD_{band}"][qso_number, :]
-y = totaldat[f"MAG_{band}"][qso_number, :]
-err = totaldat[f"MAG_ERR_{band}"][qso_number, :]
+    t = totaldat[f"MJD_{band}"][qso_number, :]
+    y = totaldat[f"MAG_{band}"][qso_number, :]
+    err = totaldat[f"MAG_ERR_{band}"][qso_number, :]
 
-keep = ~np.isnan(t)
-t, y, err = t[keep], y[keep], err[keep]
+    keep = ~np.isnan(t)
+    t, y, err = t[keep], y[keep], err[keep]
 
-## Center the data
-#w = 1.0/err
-#y -= np.sum(w*y)/np.sum(w)
+    ## Center the data
+    #w = 1.0/err
+    #y -= np.sum(w*y)/np.sum(w)
 
-data = np.empty((len(t), 3))
-data[:,0] = t
-data[:,1] = y
-data[:,2] = err
-np.savetxt("data.txt", data)
+    data = np.empty((len(t), 3))
+    data[:,0] = t
+    data[:,1] = y
+    data[:,2] = err
+    np.savetxt("data.txt", data)
 
-# Get reported tau values
-log_tau = totaldat[f"log_TAU_OBS_{band}"][qso_number]
-lower = totaldat[f"log_TAU_OBS_{band}_ERR_L"][qso_number]
-upper = totaldat[f"log_TAU_OBS_{band}_ERR_U"][qso_number]
+    # Get reported tau values
+    log_tau = totaldat[f"log_TAU_OBS_{band}"][qso_number]
+    lower = totaldat[f"log_TAU_OBS_{band}_ERR_L"][qso_number]
+    upper = totaldat[f"log_TAU_OBS_{band}_ERR_U"][qso_number]
 
-print(log_tau, lower, upper)
+    print(log_tau, lower, upper)
 
-plt.errorbar(t, y, yerr=err, fmt=".")
-plt.show()
+    plt.errorbar(t, y, yerr=err, fmt=".")
+    plt.show()
+
+    return data
+
+if __name__ == "__main__":
+    get_data(0, "g")
+
 
 fits_file.close()
+
 
