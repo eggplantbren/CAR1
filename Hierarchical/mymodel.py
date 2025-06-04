9ed89cad7d5bc536dc06fe96bdad125633d388fe
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-05 07:57:01 +1200

    Load all posterior samples files

--- Hierarchical/mymodel.py
+++ Hierarchical/mymodel.py
@@ -1,7 +1,24 @@
 import numpy as np
+import os
 
 
 num_params = 10
+
+# Load posterior samples
+files = os.listdir("../output/")
+all_samples = []
+for f in files:
+    all_samples.append(np.loadtxt("../output/" + f))
+
+
+def logsumexp(xs):
+    top = np.max(xs)
+    return (top + np.log(np.sum(np.exp(xs - top))))
+
+def logmeanexp(xs):
+    return(logsumexp(xs) - np.log(len(xs)))
+
+
 
 def prior_transform(us):
     return us - 0.5
