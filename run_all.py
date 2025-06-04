192ef7029c22d89c77e93d86ee7038435193021c
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-05 09:57:23 +1200

    RNG seeds for reproducibility

--- run_all.py
+++ run_all.py
@@ -4,11 +4,13 @@ import subprocess
 
 subprocess.run(["mkdir", "output"])
 
+seed = 0
 for i in range(190):
     for band in ["g"]:
         get_data(i, band, deredshift=True, sanitise=True, plot=False)
-        subprocess.run("./main")
-        dn4.postprocess(plot=False)
+        subprocess.run("./main -s {seed}".format(seed=seed), shell=True)
+        seed += 1
+        dn4.postprocess(plot=False, rng_seed=0)
         subprocess.run(["mv", "posterior_sample.txt",
                         f"output/posterior_sample_{i}_{band}.txt"])
 
