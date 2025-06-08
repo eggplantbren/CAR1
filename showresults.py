c34267d733c26ea2235bf03be46d28fec7e1bac2
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-09 09:37:33 +1200

    Make a cornerplot

--- showresults.py
+++ showresults.py
@@ -1,3 +1,31 @@
 import dnest4.classic as dn4
 dn4.postprocess()
 
+import corner
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.size": 14,
+})
+
+ndim = 4
+
+posterior_sample = np.loadtxt("posterior_sample.txt")
+posterior_sample[:, 1:] = np.log10(posterior_sample[:, 1:])
+
+figure = corner.corner(posterior_sample,
+    labels=["$\\mu$", "$\\log_{10}(\\sigma)$", "$\\log_{10}(\\tau)$",
+            "$\\log_{10}$(jitter)"],
+            plot_contours=False,
+            plot_density=False, fontsize=14,
+            hist_kwargs={"color":"blue", "alpha":0.3, "histtype":"stepfilled",
+                         "edgecolor":"black","lw":"3"} )
+
+axes = np.array(figure.axes).reshape((ndim, ndim))
+
+for i in range(ndim):
+	ax = axes[i,i]
+
+plt.show()
