5e038036d5d5e14e985c2799ac4df7b697d06337
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-09 09:16:05 +1200

    Nice-ish plot

--- simulate.py
+++ simulate.py
@@ -4,25 +4,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as rng
 
-rng.seed(123)
-a = 0.99
-sigma = 3.0
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.size": 14,
+})
+
+rng.seed(12)
+a = 0.99999
+sigma = 10.0
 b = sigma*np.sqrt(1.0 - a**2)
 
-t = np.linspace(0.0, 1000.0, 20001)
-y = np.zeros(t.shape)
+# Must use an arange for this - I am assuming that later.
+t = np.arange(10000)
+y = np.empty(t.shape)
 
 y[0] = sigma*rng.randn()
 for i in range(1, len(t)):
     y[i] = a*y[i-1] + b*rng.randn()
-y += 5.0 + 0.1*rng.randn(len(t))
 
-data = np.empty((len(t), 3))
-data[:,0] = t
-data[:,1] = y
-data[:,2] = 0.1
-np.savetxt("data.txt", data)
+# Observed points
+t_obs = 4000 + rng.randint(1000, size=100)
+y_obs = y[t_obs]
+y_obs += 0.1*rng.randn()
 
-plt.errorbar(t, y, yerr=0.1, fmt="o")
+# Plot the curve
+plt.plot(t, y, "r-", alpha=0.5, label="Underlying curve")
+
+# Plot the data points
+plt.errorbar(t_obs, y_obs, yerr=0.1, fmt=".", label="Observations")
+
+plt.legend()
+plt.xlabel("Time $t$")
+plt.ylabel("Signal $y(t)$")
+
+# Save and display the plot
+plt.savefig("simulation.pdf")
 plt.show()
 
