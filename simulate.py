8726c0eb81bb0814d18dfbee09846cfee8ab9c2c
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-06-09 09:30:35 +1200

    Save the data

--- simulate.py
+++ simulate.py
@@ -13,6 +13,7 @@ rng.seed(12)
 a = 0.99999
 sigma = 10.0
 b = sigma*np.sqrt(1.0 - a**2)
+sig_errorbars = 0.1
 
 # Must use an arange for this - I am assuming that later.
 t = np.arange(10000)
@@ -25,13 +26,21 @@ for i in range(1, len(t)):
 # Observed points
 t_obs = 4000 + rng.randint(1000, size=100)
 y_obs = y[t_obs]
-y_obs += 0.1*rng.randn()
+y_obs += sig_errorbars*rng.randn()
+
+# Save the data
+data = np.empty((t_obs.shape[0], 3))
+indices = np.argsort(t_obs)
+data[:,0] = t_obs[indices]
+data[:,1] = y_obs[indices]
+data[:,2] = sig_errorbars
+np.savetxt("data.txt", data)
 
 # Plot the curve
 plt.plot(t, y, "r-", alpha=0.5, label="Underlying curve")
 
 # Plot the data points
-plt.errorbar(t_obs, y_obs, yerr=0.1, fmt=".", label="Observations")
+plt.errorbar(t_obs, y_obs, yerr=sig_errorbars, fmt=".", label="Observations")
 
 plt.legend()
 plt.xlabel("Time $t$")
