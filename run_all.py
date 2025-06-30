d08bf46f45b09378ab4facbc9b3e274128f3d5db
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-07-01 07:14:39 +1200

    No need for seed when i exists

--- run_all.py
+++ run_all.py
@@ -4,12 +4,10 @@ import subprocess
 
 subprocess.run(["mkdir", "output"])
 
-seed = 0
 for i in range(190):
     for band in ["g"]:
         get_data(i, band, deredshift=True, sanitise=True, plot=False)
-        subprocess.run("./main -s {seed}".format(seed=seed), shell=True)
-        seed += 1
+        subprocess.run("./main -s {seed}".format(seed=i), shell=True)
         dn4.postprocess(plot=False, rng_seed=0)
         subprocess.run(["mv", "posterior_sample.txt",
                         f"output/posterior_sample_{i}_{band}.txt"])
