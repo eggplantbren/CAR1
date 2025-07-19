9b835c2b8451c86d918a077e943f05eed153ea14
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-07-19 13:10:15 +1200

    Looks good but probably has errors

--- Hierarchical/mymodel.py
+++ Hierarchical/mymodel.py
@@ -4,13 +4,29 @@ import pandas as pd
 
 num_params = 11
 
-# Load posterior samples
-files = os.listdir("../output/")
+# Load quasar metadata
+qso_info = pd.read_csv("../qso_info.csv")
 all_samples = []
-for f in files:
-    samples = np.loadtxt("../output/" + f)
-    all_samples.append(samples)
 
+# Construct the posterior sample filenames and attempt to load them all,
+# in the same order as they appear in qso_info.csv.
+success = []
+for i in range(qso_info.shape[0]):
+    try:
+        qso_number, band = qso_info.iloc[i, 0:2]
+        filename = "../output/posterior_sample_" + str(qso_number) + "_" + str(band) + ".txt"
+        samples = np.loadtxt(filename)
+        all_samples.append(samples)
+        success.append(i)
+        print("Loaded samples from file " + filename, flush=True)
+    except:
+        pass
+
+qso_info = qso_info.iloc[success, :]
+assert(qso_info.shape[0] == len(all_samples))
+
+mean_log10_lbol = np.mean(qso_info["log10_lbol"])
+mean_log10_lambda = np.mean(qso_info["log10_lambda"])
 
 def logsumexp(xs):
     top = np.max(xs)
@@ -19,8 +35,6 @@ def logsumexp(xs):
 def logmeanexp(xs):
     return(logsumexp(xs) - np.log(len(xs)))
 
-# Load quasar metadata
-qso_info = pd.read_csv("../qso_info.csv")
 
 
 
@@ -52,7 +66,7 @@ def prior_transform(us):
     return params
 
 
-def log_conditional_prior(params, samples):
+def log_conditional_prior(params, samples, i):
 
     # Just normal distributions
     logp = 0.0
@@ -63,8 +77,15 @@ def log_conditional_prior(params, samples):
     logp += -0.5*np.log(2.0*np.pi*params[3]**2) \
                     - 0.5*(samples[:,1] - params[2])**2/params[3]**2
 
+    # Compute expected value of log10_tau according to regression model
+    mu = params[4] + 1.0*np.log10(1.0 + qso_data["redshift"][i]) \
+                   + params[8]*(qso_data["log10_lbol"][i] - mean_log10_lbol) \
+                   + params[9]*(qso_data["log10_lambda"][i] - mean_log10_lambda) \
+                   + params[10]*(qso_data["log10_lbol"][i] - mean_log10_lbol) \
+                               *(qso_data["log10_lambda"][i] - mean_log10_lambda)
+
     logp += -0.5*np.log(2.0*np.pi*params[5]**2) \
-                    - 0.5*(samples[:,2] - params[4])**2/params[5]**2
+                    - 0.5*(samples[:,2] - mu)**2/params[5]**2
 
     logp += -0.5*np.log(2.0*np.pi*params[7]**2) \
                     - 0.5*(samples[:,3] - params[6])**2/params[7]**2
@@ -103,13 +124,15 @@ def log_interim_prior(params, samples):
 def log_likelihood(params):
     logl = 0.0
 
+    i = 0
     for samples in all_samples:
 
         # The expectation inside the product sign
-        loge = logmeanexp(log_conditional_prior(params, samples) -
+        loge = logmeanexp(log_conditional_prior(params, samples, i) -
                           log_interim_prior(params, samples))
 
         logl = logl + loge
+        i += 1
 
     return logl
 
