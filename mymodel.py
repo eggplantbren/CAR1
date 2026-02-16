f765ec14d3f7a2dcf0a2493dc77983c1e28fa1bc
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-02-16 15:45:21 +1300

    New model

--- mymodel.py
+++ mymodel.py
@@ -1,37 +1,59 @@
+from extract_data import *
 import celerite2
 from celerite2 import terms
 import numpy as np
 from scipy.stats import t
 
-num_params = 4
-data = np.loadtxt("data.txt")
+
+num_qsos = 190
+num_bands = 3
+num_hyperparameters = 0
+
+data = []
+for i in range(num_qsos):
+    for band in ["g", "r", "i"]:
+        data.append(get_data(i, band, sanitise=True, plot=False))
+
+num_params = num_hyperparameters + 4*num_qsos*num_bands
 
 def prior_transform(us):
     """
-    Parameters are mu, sigma, tau, jitter
+    Parameters are mu, sigma, tau, jitter for each light curve
     """
-    params = us.copy()
-    params[0] = 20.0 + 10.0*t.ppf(us[0], df=4) # Mean magnitude
-    params[1] = 0.0  + 5.0*t.ppf(us[1], df=4)  # log10_beta in magnitudes
-    params[2] = 0.0  + 5.0*t.ppf(us[2], df=4)  # log10_tau in days
-    params[3] = 0.0  + 5.0*t.ppf(us[3], df=4)  # log10_jitter in magnitudes
+    hypers = us[0:num_hyperparameters]
+    params = us[num_hyperparameters:]
+    params = params.reshape((num_qsos, num_bands, 4))
+
+    for i in range(num_qsos):
+        for j in range(num_bands):
+            params[i, j, 0] = 20.0 + 5.0*t.ppf(params[i, j, 0], df=4) # Mean magnitude
+            params[i, j, 1] = 0.0  + 5.0*t.ppf(params[i, j, 1], df=4) # log10_beta in magnitudes
+            params[i, j, 2] = 0.0  + 5.0*t.ppf(params[i, j, 2], df=4) # log10_tau in days
+            params[i, j, 3] = 0.0  + 5.0*t.ppf(params[i, j, 3], df=4) # log10_jitter in magnitudes
+
     return params
 
 def log_likelihood(params):
-    mu, log10_beta, log10_tau, log10_jitter = params
-    beta = 10.0**log10_beta
-    tau   = 10.0**log10_tau
-    jitter= 10.0**log10_jitter
-    sigma = beta*np.sqrt(0.5*tau)
 
-    try:
-        term = terms.RealTerm(a=sigma**2, c=1.0/tau)
-        kernel = term
-        gp = celerite2.GaussianProcess(kernel, mean=mu)
-        gp.compute(data[:,0], yerr=np.sqrt(data[:,2]**2 + jitter**2))
-        logl = gp.log_likelihood(data[:,1])
-    except:
-        logl = -1.0E300
+    logl = 0.0
+    k = 0
+    for i in range(num_qsos):
+        for j in range(num_bands):
+            mu =    params[i, j, 0]   
+            beta =  10.0**params[i, j, 1]
+            tau   = 10.0**params[i, j, 2]
+            jitter= 10.0**params[i, j, 3]
+            sigma = beta*np.sqrt(0.5*tau)
+
+            try:
+                term = terms.RealTerm(a=sigma**2, c=1.0/tau)
+                kernel = term
+                gp = celerite2.GaussianProcess(kernel, mean=mu)
+                gp.compute(data[k][:,0], yerr=np.sqrt(data[k][:,2]**2 + jitter**2))
+                logl += gp.log_likelihood(data[k][:,1])
+            except:
+                logl += -1.0E300
+            k += 1
 
     return logl
 
