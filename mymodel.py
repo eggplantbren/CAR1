29911ca1e6d0f3bfd00105406546392a94c40a1d
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-02-16 16:22:19 +1300

    Vectorisation

--- mymodel.py
+++ mymodel.py
@@ -24,33 +24,41 @@ def prior_transform(us):
     params = us[num_hyperparameters:].copy()
     params = params.reshape((num_qsos, num_bands, 4))
 
-    for i in range(num_qsos):
-        for j in range(num_bands):
-            params[i, j, 0] = 20.0 + 5.0*t.ppf(params[i, j, 0], df=4) # Mean magnitude
-            params[i, j, 1] = 0.0  + 5.0*t.ppf(params[i, j, 1], df=4) # log10_beta in magnitudes
-            params[i, j, 2] = 0.0  + 5.0*t.ppf(params[i, j, 2], df=4) # log10_tau in days
-            params[i, j, 3] = 0.0  + 5.0*t.ppf(params[i, j, 3], df=4) # log10_jitter in magnitudes
+    params[:, :, 0] = 20.0 + 5.0*t.ppf(params[:, :, 0], df=4) # Mean magnitude
+    params[:, :, 1] = 0.0  + 5.0*t.ppf(params[:, :, 1], df=4) # log10_beta in magnitudes
+    params[:, :, 2] = 0.0  + 5.0*t.ppf(params[:, :, 2], df=4) # log10_tau in days
+    params[:, :, 3] = 0.0  + 5.0*t.ppf(params[:, :, 3], df=4) # log10_jitter in magnitudes
+
+    # 161 ms
+#    for i in range(num_qsos):
+#        for j in range(num_bands):
+#            params[i, j, 0] = 20.0 + 5.0*t.ppf(params[i, j, 0], df=4) # Mean magnitude
+#            params[i, j, 1] = 0.0  + 5.0*t.ppf(params[i, j, 1], df=4) # log10_beta in magnitudes
+#            params[i, j, 2] = 0.0  + 5.0*t.ppf(params[i, j, 2], df=4) # log10_tau in days
+#            params[i, j, 3] = 0.0  + 5.0*t.ppf(params[i, j, 3], df=4) # log10_jitter in magnitudes
 
     return params
 
 def log_likelihood(params):
 
     logl = 0.0
+
+    mu =    params[:, :, 0]   
+    beta =  10.0**params[:, :, 1]
+    tau   = 10.0**params[:, :, 2]
+    jitter= 10.0**params[:, :, 3]
+    sigma = beta*np.sqrt(0.5*tau)
+
     k = 0
     for i in range(num_qsos):
         for j in range(num_bands):
-            mu =    params[i, j, 0]   
-            beta =  10.0**params[i, j, 1]
-            tau   = 10.0**params[i, j, 2]
-            jitter= 10.0**params[i, j, 3]
-            sigma = beta*np.sqrt(0.5*tau)
 
             try:
-                term = terms.RealTerm(a=sigma**2, c=1.0/tau)
+                term = terms.RealTerm(a=sigma[i, j]**2, c=1.0/tau[i, j])
                 kernel = term
-                gp = celerite2.GaussianProcess(kernel, mean=mu)
+                gp = celerite2.GaussianProcess(kernel, mean=mu[i, j])
                 this_data = data[k]["light_curve"]
-                gp.compute(this_data[:,0], yerr=np.sqrt(this_data[:,2]**2 + jitter**2))
+                gp.compute(this_data[:,0], yerr=np.sqrt(this_data[:,2]**2 + jitter[i, j]**2))
                 logl += gp.log_likelihood(this_data[:,1])
             except:
                 logl += -1.0E300
